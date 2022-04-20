ecc7e3ebb3f93c2ae074835b6508a95f435f50a2
Imam Asari <imam.as@treeclouds.com>
2022-04-20 23:17:13 +0700

    fix import local settings 2

--- customPOS/settings.py
+++ customPOS/settings.py
@@ -77,15 +77,6 @@ WSGI_APPLICATION = 'customPOS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-try:
-    from .local_settings2 import *
-except ImportError:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 # Password validation
@@ -138,3 +129,16 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+try:
+    from .local_settings2 import *
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    
